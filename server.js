18d1c07aad2c37750889ae9e341d622e380a1260
Sathishramesh1 <sathishrameshkec@gmail.com>
2023-10-09 19:12:26 +0530

    second-commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const {  room, booking } = require("./data.js");
 require('dotenv').config()
 
 const app=express();
@@ -7,53 +8,123 @@ app.use(bodyParser.json());
 const PORT=process.env.PORT
 
 app.get("/",(req,res)=>{
-    res.status(201).json(room);
+    res.status(201).send(`hall booking server working fine`);
 
 
 });
 
-const room=[];
-const booking=[]
 
-//api for room creation
+//1.api for room creation
 app.post("/createroom",(req,res)=>{
 const newroom=req.body;
-if(!newroom.id || !newroom.available_seats || ! newroom.price_perhour){
-    return res.status(400).send("please enter id,avialble_seats,price");
+if( !newroom.roomName || ! newroom.seats ||!newroom. amenities ||!newroom.pricePerHour){
+    return res.status(400).send("please roomName,seats,amenities,pricePerHour");
 }
+newroom.roomId=room.length+1;
 room.push(newroom);
 res.status(201).send(`room created successfully`);
-})
+});
 
 
-//api for booking room
+//2.api for booking room
 app.post("/bookroom",(req,res)=>{
-    const book_room=req.body;
+    const book_room={
+           roomId:req.body.roomId,
+           customerName:req.body.customerName,
+           date:new Date(req.body.date).toLocaleDateString(),
+           startTime:req.body.startTime.toFixed(2),
+           endTime:req.body.endTime.toFixed(2)
 
-if(!book_room.id||!book_room.customer_name ||!book_room.date ){
-    return res.status(400).send("please enter details");
-}else {
-const  booking_condtion=room.some((e)=>e.id==book_room.id)
- const overlap_booking=booking.every((e)=>e?.id !== book_room.id);
+    };
 
-if(booking_condtion&&overlap_booking){
-    booking.push(book_room);
-    return res.status(201).send("room booked");
-}else{
-    return res.status(400).send("room already booked");
-} 
+if(!book_room.roomId||!book_room.customerName ||!book_room.date
+     ||!book_room.startTime ||!book_room.endTime){
+    return res.status(400).send(`please enter roomId,
+    customerName,date(yyyy-mm-dd),startTime,Endtime in 24hrs format
+    
+    `);
+}
+else {
+ //checking room avilablity
+
+const availablity=booking.filter((e)=>e.roomId==book_room.roomId).every((e)=>e?.date!==book_room.date ||(e.startTime>book_room.startTime &&e.endTime>book_room.endTime) ||(e.endTime<book_room.startTime ))
+
+ if(room.findIndex(e=>e.id==book_room.roomId)!==-1 && availablity){
+    book_room.id=booking.length+1;
+    booking.push(book_room); 
+    return res.status(201).json("room booked sucessfully");
+ }else{
+    return res.status(400).send('slot not avilable')
+ }
 
 }
 
 });
 
-//list all room with booked data
+//3.list all room with booked data
 app.get("/allrooms",(req,res)=>{
-    return res.status(201).json(booking.map((e)=>e))
+    const roomBookings = room.map(room => {
+        const bookingsForRoom = booking.filter(booking => booking.roomId === room.id);
+        const bookedStatus = bookingsForRoom.length > 0 ? 'Booked' : 'Available'; 
+        return {
+          roomName: room.roomName,
+          bookedStatus,
+          bookings: bookingsForRoom
+        };
+      });
+    
+      res.status(200).json(roomBookings);
+    
 })
 
+
+
+//4.list the all customer with booked data
+
+app.get("/allcustomers",(req,res)=>{
+
+    const customerBookings = booking.map(booking => {
+        const rooms = room.find(room => room.id === booking.roomId);
+    
+        return {
+          customerName: booking.customerName,
+          roomName: rooms.roomName,
+          date: booking.date,
+          startTime: booking.startTime,
+          endTime: booking.endTime
+        };
+      });
+    
+      res.status(200).json(customerBookings);
+
+});
+
+//5.list how many times customer has booked the room with
+//  following details
+
+app.get("/bookingcount/:customerName",(req,res)=>{
+ 
+    const { customerName } = req.params;
+
+  const customerBookings = booking.filter(booking => booking.customerName === customerName);
+
+  res.status(200).json({ 
+    customerName:customerName,
+    bookingCount: customerBookings.length,
+    bookings: customerBookings
+  });
+    
+ 
+});
+
+
+
+
 app.listen(PORT,()=>{
-    console.log(`The server running on port ${PORT}`)
+    console.log(`The server running on port ${PORT}`);
+    
+    
+    
 })
 
 
